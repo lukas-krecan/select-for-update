f86fecf03694cca40432c006b682c9ebb5f55ad7
Lukas Krecan <lukas@krecan.net>
2022-10-14 21:16:00 +0200

    Simulate bug

--- src/test/java/JdbcTest.java
+++ src/test/java/JdbcTest.java
@@ -1,6 +1,7 @@
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
+import org.postgresql.util.PGobject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -9,13 +10,11 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.stream.IntStream;
+import java.util.Random;
 
 @Testcontainers
 public class JdbcTest {
@@ -24,13 +23,11 @@ public class JdbcTest {
 
     private final DataSource dataSource;
 
-    private final ExecutorService executorService = Executors.newFixedThreadPool(20);
-
     private static final Logger logger = LoggerFactory.getLogger(JdbcTest.class);
 
     public JdbcTest() {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl(container.getJdbcUrl());
+        hikariConfig.setJdbcUrl(container.getJdbcUrl() + "?maxResultBuffer=10M");
         hikariConfig.setUsername(container.getUsername());
         hikariConfig.setPassword(container.getPassword());
         hikariConfig.setDriverClassName(container.getDriverClassName());
@@ -41,41 +38,53 @@ public class JdbcTest {
 
     @Test
     void runTest() throws Exception {
-        performQuery("CREATE TABLE queue (id INTEGER PRIMARY KEY, status CHAR(10))");
-        performQuery("INSERT INTO queue VALUES (1, 'PENDING')");
-        performQuery("INSERT INTO queue VALUES (2, 'PENDING')");
-        performQuery("INSERT INTO queue VALUES (3, 'PENDING')");
+        String data = "{\"test\": \"" + randomString(1_000_000) + "\"}";
 
-        List<Callable<Integer>> tasks = IntStream.range(0, 20).mapToObj(i -> (Callable<Integer>) this::runSelectForUpdate).toList();
-
-        executorService.invokeAll(tasks).forEach(future -> {
-            try {
-                logger.info("Updated " + future.get());
-            } catch (Exception e) {
-                logger.info("Exception " + e.getMessage());
-            }
-        });
+        createTable();
+        
+        for (int i = 0; i < 100; i++) {
+            insert( i , data);
+            logger.info("inserted {}", i);
+        }
+        doSelect();
     }
 
-    private int runSelectForUpdate() throws Exception {
-        int updatedRows = 0;
-        try (Connection connection = dataSource.getConnection()) {
-            connection.setAutoCommit(false);
-            try (Statement statement = connection.createStatement()) {
-                statement.execute("SELECT id, status FROM queue WHERE status = 'PENDING' LIMIT 2 FOR UPDATE NOWAIT");
-            }
-            Thread.sleep(100);
-            try (Statement statement = connection.createStatement()) {
-                updatedRows = statement.executeUpdate("UPDATE queue SET status = 'PROCESSED' WHERE status = 'PENDING'");
-            }
-            connection.commit();
+    private void createTable() throws SQLException {
+        try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE queue (id INTEGER PRIMARY KEY, status JSONB)");
         }
-        return updatedRows;
     }
 
-    protected void performQuery(String sql) throws SQLException {
-        try (Statement statement = dataSource.getConnection().createStatement()) {
-            statement.execute(sql);
+
+    protected void insert(int i, String data) throws SQLException {
+        PGobject jsonObject = new PGobject();
+        jsonObject.setType("json");
+        jsonObject.setValue(data);
+
+        try (Connection connection = dataSource.getConnection(); PreparedStatement statement = connection.prepareStatement("insert into queue values(?, ?)")) {
+            statement.setInt(1, i);
+            statement.setObject(2, jsonObject);
+            statement.executeUpdate();
         }
+    }
+
+    protected void doSelect() throws SQLException {
+        try (Connection connection = dataSource.getConnection(); Statement statement = connection.createStatement()) {
+            statement.execute("SELECT id, status FROM queue");
+            try (ResultSet rs = statement.getResultSet()) {
+                while (rs.next()) ;
+            }
+        }
+    }
+    private String randomString(int targetStringLength) {
+        int leftLimit = 48; // numeral '0'
+        int rightLimit = 122; // letter 'z'
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+            .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+            .limit(targetStringLength)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
     }
 }
